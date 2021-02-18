84bac1610c7a63e53d6fd57476a748e91c67be32
tauseef <tauseef.ahmad@contentsphere.com>
2021-02-18 19:12:35 +0530

    insta_login introduction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import InstagramLogin from 'react-instagram-login';
 
 function App() {
+  const responseInstagram = (response) => {
+    console.log(response);
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
+      <InstagramLogin
+        clientId="473669417373668"
+        buttonText="Login"
+        onSuccess={responseInstagram}
+        onFailure={responseInstagram}
+      />
     </div>
   );
 }
