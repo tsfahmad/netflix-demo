8b50fcdca921a9a93f2293356831b1db174bcb0a
tauseef <tauseef.ahmad@contentsphere.com>
2021-02-19 12:39:07 +0530

    URL issue fix

--- src/components/media-list.js
+++ src/components/media-list.js
@@ -5,7 +5,7 @@ const MediaList = ({user_id, access_token}) => {
     const [list, setList] = useState([]);
 
     useEffect(() => {
-        fetch(`https://graph.instagram.com/${user_id}}/media?access_token=${access_token}`)
+        fetch(`https://graph.instagram.com/${user_id}/media?access_token=${access_token}`)
         .then(response => response.json())
         .then(response =>setList(response.data));
     }, [access_token, user_id]);
