f7e2c3a48c1508b40d73b1320a51c69e5ea7e6d8
tauseef <tauseef.ahmad@contentsphere.com>
2021-02-19 10:57:35 +0530

    Get access token

--- src/components/login.js
+++ src/components/login.js
@@ -7,7 +7,24 @@ const Login = ({clientId, scope, onSuccess}) => {
         const redirectUri = window.location.href
         if (window.location.search.includes('code')) {
             const urlParams = new URLSearchParams(window.location.search);
-            onSuccess(urlParams.get('code'));
+            const code = urlParams.get('code');
+            const data = {
+              client_id: clientId,
+              client_secret: '37486c235230f8ae895e3586b5a78c9c',
+              grant_type: 'authorization_code',
+              redirect_uri: redirectUri,
+              code: code
+            };
+
+            fetch('https://api.instagram.com/oauth/access_token', {
+              method: 'POST',
+              mode: 'cors',
+              body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(data => console.log(data));
+
+            onSuccess(code);
           } 
           else {
             window.location.href = `https://api.instagram.com/oauth/authorize/?app_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`
