823f8e171655f1a2a82b3a1876ea2dfe1a715fa7
tauseef <tauseef.ahmad@contentsphere.com>
2021-02-19 14:50:30 +0530

    Added Media url for images

--- src/components/media.js
+++ src/components/media.js
@@ -4,19 +4,19 @@ const Media = ({media_id, access_token}) => {
     const [media, setMedia] = useState(null);
 
     useEffect(() => {
-        fetch(`https://graph.instagram.com/${media_id}?fields=id,media_type,media_url,thumbnail_url,username,timestamp&access_token=${access_token}`)
+        fetch(`https://graph.instagram.com/${media_id}?fields=id,media_type,media_url,username,timestamp&access_token=${access_token}`)
         .then(response => response.json())
         .then(data =>setMedia(data));
     }, [access_token, media_id]);
 
     const getMedia = () => {
         if(media) {
-            const {media_type, media_url, thumbnail_url, caption} = media;
+            const {media_type, media_url, caption} = media;
             switch(media_type) {
                 case 'IMAGE':
-                    return <img src={thumbnail_url} alt={caption}/>
+                    return <img src={media_url} alt={caption}/>
                 case 'VIDEO':
-                    return <video src={media_url}/>
+                    return <video src={media_url} controls/>
                 default:
                     return null;    
             }
